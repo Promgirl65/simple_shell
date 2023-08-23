4c1fb4f2d678e4ea87e569a72eba4790e148556b
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-08-23 16:56:02 -0700

    bettty

---
+++ bettty.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * sum - the summation of num 1 and num 2
+ *@ num 1 :the number to be added
+ *@num 2:the number to be added
+ *Return:always 0
+ */
+
+int main(void)
+{
+	int num 1 = 6;
+	int num 2 = 3;
+	float sum = (float) num1/num2;
+	printf("%f", sum);
+	Return 0;
+}
